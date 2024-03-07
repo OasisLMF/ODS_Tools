7b01ea00397b0c14f4bc65e7a3184e6b8616b096
ncerutti <nc@nicores.de>
2024-03-07 18:15:21 +0000

    ignore missing columns

--- ods_tools/odtf/runner/pandas.py
+++ ods_tools/odtf/runner/pandas.py
@@ -4,6 +4,7 @@ import os
 import re
 from functools import reduce
 from operator import and_, or_
+from lark import Tree
 from typing import Any, Dict, Iterable, Union
 
 import pandas as pd
@@ -422,6 +423,12 @@ class PandasRunner(BaseRunner):
             return result
         else:
             return self.create_series(input_df.index, result)
+
+    def extract_column_name(self, transformation_tree):
+        if isinstance(transformation_tree, Tree):
+            if transformation_tree.data == 'lookup':
+                return transformation_tree.children[0].value
+        return ""
 
     def transform(
         self, extractor: BaseConnector, mapping: BaseMapping
