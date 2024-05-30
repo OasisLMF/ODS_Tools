d0a833c74cb4c7df0c21aa55713b599268151d1c
Stephane Struzik <34556337+sstruzik@users.noreply.github.com>
2024-05-30 06:35:03 +0100

    Fix/fill empty (#117)
    * make fill_empty faster and reliable
    
    * improve empty val managment
    
    * fixup
    
    * fixup

--- tests/test_ods_package.py
+++ tests/test_ods_package.py
@@ -407,7 +407,11 @@ class OdsPackageTests(TestCase):
             'use_field': True})
 
         original_exposure.location.dataframe.drop(columns=['ContentsTIV'], inplace=True)
-        original_exposure.location.dataframe['BITIV'] = np.nan
+        original_exposure.location.dataframe['BITIV'] = pd.NA
+        original_exposure.location.dataframe.loc[[1], 'BITIV'] = np.nan
+        original_exposure.location.dataframe.loc[[2], 'BITIV'] = None
+        original_exposure.location.dataframe.loc[[3], 'BITIV'] = pd.NaT
+        original_exposure.location.dataframe.loc[[4], 'BITIV'] = ''
         original_exposure.ri_info.dataframe.drop(columns='RiskLevel', inplace=True)
 
         with tempfile.TemporaryDirectory() as tmp_dir:
