fb0da99d38a0f7d19bd4770cea15568054099c10
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-10-24 11:12:49 +0100

    implement loss sampling - beta, mean only

--- ord_combining/loss_sampling.py
+++ ord_combining/loss_sampling.py
@@ -26,6 +26,10 @@ event_occurrence_set_df = pd.read_csv(output_path / 'event_occurrence_set.csv')
 group_period_df = pd.read_csv(output_path / 'group_period.csv')
 output_set_df = pd.read_csv(output_path / 'output_set.csv' )
 
+config = {
+        'mean_only': True
+        }
+
 # %% generate group period quantile table
 def load_ord_lt(analysis, occurence_file=False,
                 format_priority=['M', 'Q', 'S'],
@@ -48,7 +52,7 @@ def load_ord_lt(analysis, occurence_file=False,
 
 # %% ## generate quantiles
 
-def construct_gpqt(group_period_df, group_event_set_analysis_df, output_set_df):
+def construct_gpqt(group_period_df, group_event_set_analysis_df, output_set_df, mean_only=False):
 
     gpqt_fragments = []
 
@@ -73,40 +77,48 @@ def construct_gpqt(group_period_df, group_event_set_analysis_df, output_set_df):
 
             plt_df = pd.read_csv(plt_path)
 
-            period_event_df = plt_df[['Period', 'EventId']].drop_duplicates() # might want to do the outputset sampling calc here
+            plt_df = plt_df.query("SampleType == 2")
+
+            period_event_df = plt_df[['Period', 'EventId', 'MeanLoss', 'SDLoss', 'MaxLoss']]
 
             _gpqt_fragment = filtered_group_period.merge(period_event_df, on='Period',
                                                          how='inner')
 
-            _gpqt_fragment['Quantile'] = rng.random(size=len(_gpqt_fragment))
 
             _gpqt_fragment['output_set_id'] = curr_os['id']
-            _gpqt_fragment['output_type'] = plt_path.stem[-4]
+            if mean_only:
+                _gpqt_fragment['Quantile'] = None
+                _gpqt_fragment['output_type'] = 'mean'
+            else:
+                _gpqt_fragment['Quantile'] = rng.random(size=len(_gpqt_fragment))
+                _gpqt_fragment['output_type'] = plt_path.stem[-4]
 
             gpqt_fragments.append(_gpqt_fragment)
 
     return pd.concat(gpqt_fragments)
+
+
+gpqt = construct_gpqt(group_period_df, group_event_set_analysis_df, output_set_df, config.get('mean_only', False))
+
+gpqt.to_csv(output_path / "group_period_quantile_1.csv", index=False)
 
 gpqt_dtype = {
         'GroupPeriod': np.int32,
         'Period': np.int32,
         'group_event_set_id': np.int32,
         'EventId': np.int32,
-        'Quantile': np.float32,
         'output_set_id': np.int32
         }
 
-gpqt = construct_gpqt(group_period_df, group_event_set_analysis_df, output_set_df)
-
-gpqt.to_csv(output_path / "group_period_quantile_1.csv", index=False)
-
+if 'Quantile' in gpqt:
+    gpqt_dtype['Quantile'] = np.float32,
 
 gpqt.astype(gpqt_dtype)
 print(gpqt)
 
 # %% loss sampling MELT
 
-def perform_beta_parameterisation(df):
+def beta_sampling_group_loss(df):
     df['mu'] = df['MeanLoss'] / df['MaxLoss'] # TODO need to verify form of this - also should we use MaxImpactedExposure as outlined in joh's sheet?
     df['sigma'] = df['SDLoss'] / df['MaxLoss']
 
@@ -115,49 +127,40 @@ def perform_beta_parameterisation(df):
 
     df_filter = df[['alpha', 'beta']].notna().all(axis='columns')
 
-    df.loc[df_filter, 'sampled'] = df.loc[df_filter, 'MaxLoss']*betaincinv(df[df_filter]['alpha'], df[df_filter]['beta'], 0.5)
+    df.loc[df_filter, 'Loss'] = df.loc[df_filter, 'MaxLoss']*betaincinv(df[df_filter]['alpha'], df[df_filter]['beta'], df[df_filter]['Quantile'])
 
-    df.loc[~df_filter, 'sampled'] = df.loc[~df_filter, 'MeanLoss'] # default to MeanLoss
+    df.loc[~df_filter, 'Loss'] = df.loc[~df_filter, 'MeanLoss'] # default to MeanLoss
+
+    df = df.drop(columns=['alpha', 'beta', 'mu', 'sigma'])
 
     return df
 
+def mean_only_group_loss(df):
+    df['Loss'] = df['MeanLoss']
+    return df
 
-def loss_sample_melt(gpqt, output_set_df, analysis):
-    assert (gpqt['output_type'] == 'm').all()
+def do_loss_sampling(gpqt):
+    '''Perform the loss sampling on gpqt'''
 
-    for output_set_id in gpqt['output_set_id'].unique():
-        filtered_gpqt = gpqt.query(f'output_set_id == {output_set_id}')
-        curr_os = output_set_df.loc[output_set_id]
+    map_output_type_to_func = {
+            'm': beta_sampling_group_loss,
+            'mean': mean_only_group_loss
+            }
 
-        curr_analysis = analysis[curr_os['analysis_id']]
+    loss_sample_fragments = []
+    for output_type in gpqt['output_type'].unique():
+        print("Running output_type: ", output_type)
+        loss_func = map_output_type_to_func.get(output_type, None)
 
-        plt_path = load_ord_lt(curr_analysis,
-                             occurence_file=False,
-                             summary_id=curr_os['exposure_summary_level_id'],
-                             perspective=curr_os['perspective_code']
-                             )[0]
+        if loss_func is None:
+            print(f"output_type: {output_type} not recognised.")
+            raise Exception("Output type not recognised.")
 
-        plt_df = pd.read_csv(plt_path)
-        plt_df = plt_df.query("SampleType == 2")
+        filtered_gpqt = gpqt.query(f'output_type == "{output_type}"')
+        loss_sample_fragments.append(loss_func(filtered_gpqt))
 
-        duplicated_df = plt_df[plt_df[['Period', 'EventId']].duplicated()]
+    return pd.concat(loss_sample_fragments)
 
-        print(f"Current os: {output_set_id}")
-        print(f"Path: {plt_path}")
-        print(f"Duplicated: ")
-        print(duplicated_df)
-        print()
+output = do_loss_sampling(gpqt)
 
-        plt_df = plt_df.query('SampleType == 2')
-
-        plt_df = plt_df[['EventId', 'MeanLoss', 'SDLoss', 'MaxLoss']]
-
-        plt_df = perform_beta_parameterisation(plt_df)
-
-        return plt_df
-
-
-
-output = loss_sample_melt(gpqt, output_set_df, analysis_dict)
-
-output
+output[['MeanLoss', 'Quantile', 'Loss']]
