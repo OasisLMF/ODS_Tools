65dc029f1ba8b3c901bef379cf4bf5cb6bd9759d
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2026-01-06 17:25:18 +0000

    implement loading anlaysis and outputsets

---
+++ ods_tools/combine/result.py
@@ -0,0 +1,82 @@
+"""
+This module manages interfacing with ORD folders.
+"""
+from dataclasses import dataclass, asdict, field
+from pathlib import Path
+from typing import List
+
+from ods_tools.oed.setting_schema import AnalysisSettingSchema
+
+
+@dataclass
+class OutputSet():
+    '''OutputSet class'''
+    id: int = None
+    perspective_code: str = None
+    analysis_id: int = None  # link to Analysis
+    exposure_summary_level_fields: List[str] = field(default_factory=lambda: list)
+    exposure_summary_level_id: int = None
+
+
+@dataclass
+class Analysis():
+    '''Analysis class'''
+    id: int = None
+    run_id: str = None
+    description: str = ''
+    settings: dict = field(default_factory=lambda: dict)
+    outputsets: List[OutputSet] = None
+    path: str = ''
+
+    @classmethod
+    def from_analysis_settings(cls, analysis_settings, path=None, id=None):
+        '''Initialise Analysis object from analysis settings.'''
+        init_params = {}
+        init_params['id'] = id
+        init_params['run_id'] = analysis_settings.get('analysis_tag')
+        init_params['path'] = path
+        init_params['settings'] = analysis_settings
+
+        init_params['outputsets'] = cls.extract_outputsets(analysis_settings, analysis_id=id)
+        return cls(**init_params)
+
+    @staticmethod
+    def extract_outputsets(analysis_settings, analysis_id=None):
+        '''
+        Load outputsets from an analysis_settings file.
+        '''
+        perspectives = ['gul', 'il', 'ri']
+        outputset_list = []
+        for p in perspectives:
+            if not analysis_settings.get(f'{p}_output', False):
+                continue
+
+            summaries = analysis_settings.get(f'{p}_summaries', {})
+            for summary_level in summaries:
+                outputset = OutputSet()
+                outputset.perspective_code = p
+                outputset.exposure_summary_level_id = summary_level['id']
+                outputset.exposure_summary_level_fields = summary_level.get('oed_fields', [])
+                outputset.analysis_id = analysis_id
+
+                outputset_list.append(outputset)
+
+        return outputset_list if len(outputset_list) != 0 else None
+
+
+def load_analysis_dirs(analysis_dirs):
+    '''
+    Load the ORD Analyses from a list of analysis directories.
+    '''
+    analyses = []
+
+    ods_analysis_settings_schema = AnalysisSettingSchema()
+    curr_id = 1
+    for dir in analysis_dirs:
+        curr_settings = ods_analysis_settings_schema.load(Path(dir) / 'analysis_settings.json')
+        curr_analysis = Analysis.from_analysis_settings(curr_settings, path=Path(dir), id=curr_id)
+
+        analyses.append(curr_analysis)
+        curr_id += 1
+
+    return analyses
--- tests/test_combine.py
+++ tests/test_combine.py
@@ -1,8 +1,10 @@
+from dataclasses import asdict
 from pathlib import Path
 import tempfile
 import json
 
 from ods_tools.combine.combine import combine
+from ods_tools.combine.result import load_analysis_dirs
 
 
 example_path = Path(Path(__file__).parent.parent, "ods_tools", "combine", "examples")
@@ -26,3 +28,40 @@ def test_combine_as_expected():
         print(config)
 
     combine_result = combine(str(config_path))
+
+
+def test_combine__load_analysis_dirs():
+    expected_analyses = [
+        {'id': 1,
+         'outputsets': [{'id': None, 'perspective_code': 'gul',
+                        'analysis_id': 1,
+                         'exposure_summary_level_fields': [],
+                         'exposure_summary_level_id': 1},
+                        {'id': None, 'perspective_code': 'gul',
+                        'analysis_id': 1,
+                         'exposure_summary_level_fields': ['LocNumber'],
+                         'exposure_summary_level_id': 2}]
+         },
+        {'id': 2,
+         'outputsets': [{'id': None, 'perspective_code': 'gul',
+                        'analysis_id': 2,
+                         'exposure_summary_level_fields': [],
+                         'exposure_summary_level_id': 1},
+                        {'id': None, 'perspective_code': 'gul',
+                        'analysis_id': 2,
+                         'exposure_summary_level_fields': ['LocNumber'],
+                         'exposure_summary_level_id': 2}]
+         }
+    ]
+
+    analysis_dirs = [example_path / 'inputs/1', example_path / 'inputs/2']
+    analyses = load_analysis_dirs(analysis_dirs)
+
+    assert len(analyses) == len(expected_analyses)
+    for analysis, expected_analysis in zip(analyses, expected_analyses):
+        assert analysis.id == expected_analysis['id']
+        assert len(analysis.outputsets) == len(expected_analysis['outputsets'])
+
+        for outputset, expected_outputset in zip(analysis.outputsets, expected_analysis['outputsets']):
+            outputset = asdict(outputset)
+            assert outputset == expected_outputset
