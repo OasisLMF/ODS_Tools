215504120aa758da06607d114915b585767c53de
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2026-06-10 16:13:02 +0100

    support python<=3.10

--- tests/test_ods_package.py
+++ tests/test_ods_package.py
@@ -1166,17 +1166,19 @@ class OdsSettingsTests(TestCase):
         self._caplog = caplog
 
     def setUp(self):
-        super().setUp()
-        self.tmp_dir = self.enterContext(tempfile.TemporaryDirectory())
+        self.tmp_dir = tempfile.TemporaryDirectory()
+        self.addCleanup(self.tmp_dir.cleanup)
 
         # setup settings files
-        parent_dir = pathlib.Path(self.tmp_dir) / 'piwind'
+        parent_dir = pathlib.Path(self.tmp_dir.name) / 'piwind'
         parent_dir.mkdir(exist_ok=True)
 
         base_file_url = f'https://raw.githubusercontent.com/OasisLMF/OasisPiWind/{piwind_branch}'
 
         self.analysis_settings_path = self.setup_settings_path(parent_dir, base_file_url, 'analysis_settings.json')
         self.model_settings_path = self.setup_settings_path(parent_dir, base_file_url, 'meta-data/model_settings.json')
+
+        super().setUp()
 
     def setup_settings_path(self, parent_dir, base_file_url, file_name):
         fpath = parent_dir / pathlib.Path(file_name).name
